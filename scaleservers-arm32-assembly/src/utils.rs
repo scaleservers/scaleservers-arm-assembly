2c09e1c7e3b875387f7d2af49c7956a4fb09e326
kit-scaleservers <154639387+kit-scaleservers@users.noreply.github.com>
2026-07-11 18:56:59 -0400

    arm32: add ArmT32InstructionEncoder; complete utils/

--- scaleservers-arm32-assembly/src/utils.rs
+++ scaleservers-arm32-assembly/src/utils.rs
@@ -3,6 +3,9 @@
 mod armt32_instruction_decoder;
 pub use armt32_instruction_decoder::ArmT32InstructionDecoder;
 
+mod armt32_instruction_encoder;
+pub use armt32_instruction_encoder::ArmT32InstructionEncoder;
+
 pub mod gpr_coding_utils;
 
 pub mod sign_extension_utils;
